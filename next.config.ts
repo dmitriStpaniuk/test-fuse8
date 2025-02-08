c2b699d77ccd59017f093fc29efd379bb669e5a0
DmitrySterpaniuk <polevaiabrest@gmail.com>
2025-02-08 23:52:38 +0300

    fix: deploy gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // basePath: "/test-fuse8",
+  output: "export",
+  basePath: "/test-fuse8",
   images: {
-    domains: ['rickandmortyapi.com'],
+    remotePatterns: [
+      {
+        hostname: "rickandmortyapi.com",
+      },
+    ],
   },
 };
-
 
 export default nextConfig;
