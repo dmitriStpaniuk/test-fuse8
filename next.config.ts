e01c156a50d5f15d4f331e6f3c200b4835602552
DmitrySterpaniuk <polevaiabrest@gmail.com>
2025-02-09 12:40:41 +0300

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/test-fuse8",
+  // basePath: "/",
   images: {
     unoptimized: true,
     remotePatterns: [
